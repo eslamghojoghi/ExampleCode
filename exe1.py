7e1a9bf532f3e0156fbac468634b6f69f624d00f
eslamghojoghi <105107595+eslamghojoghi@users.noreply.github.com>
2022-08-15 00:39:27 +0430

    Add files via upload

---
+++ exe1.py
@@ -0,0 +1,25 @@
+a=12
+b=3
+#add
+c=a+b
+print(c)
+#power
+d=a**b
+print(d)
+
+
+f=a**(1/3)
+print(f)
+
+g=23%10*12
+print(g)
+
+h=4+5j
+print(type(h))
+
+i=3**-2
+print(i)
+j=(-3)**(1/2)
+print(type(j))
+#k=5/0
+import this
